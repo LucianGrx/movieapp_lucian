286acf2c0d5fa5c7f7a31622479f6aba65165537
luciangrx <luciangrr@gmail.com>
2024-03-05 13:37:53 +0200

    feat: I added Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { Actors, MovieInfo, Movies, Navbar, Profile } from "./components/index";
-import './styles/App.css'
- 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./styles/App.css";
+
+const theme = createTheme({});
 
 const App = () => {
   return (
-    <div className="container">
-      <CssBaseline />
-      <Navbar />
-      <main className="content">
-      <div className="toolbar" />
-        <Routes>
-          <Route path="/" element={<Movies />}/>
-          <Route path="/movie/:id" element={<MovieInfo />}/>
-          <Route path="/actors/:id" element={<Actors />}/> 
-          <Route path="/profile" element={<Profile />}/>
-        </Routes>
-      </main>
-    </div>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <CssBaseline />
+        <Navbar />
+        <main className="content">
+          <div className="toolbar" />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInfo />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,95 @@
-
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { useTheme } from "@mui/material/styles";
+import { useState } from "react";
+import { Sidebar } from "../index";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const theme = useTheme();
+  const isAuthentificated = true;
 
-export default Navbar
+  return (
+    <>
+      <AppBar posotion="fixed">
+        <Toolbar className="nav-toolbar">
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              className="nav-menuButton"
+              onClick={() => {}}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton style={{marginLeft: isMobile ? 0 : '240px'}} color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search..."}
+          <div>
+            {!isAuthentificated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={"/profile/123"}
+                className="nav-linkButton"
+                onClick={() => {}}
+              >
+                {!isMobile && <>My movies&nbsp; </>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="../../../public/images/avatar-placeholder.jpg"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className="nav-drawer">
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              className="nav-drawerPaper"
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer className="nav-drawerPaper" variant="permanent" open>
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
